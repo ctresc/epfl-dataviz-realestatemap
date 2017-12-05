090be11d655f01ad027fb7eec0f75f6bf9379842
Mateusz <paluchowski.matthew@gmail.com>
2017-12-05 11:44:17 +0100

    use housing Zillow data column POC

--- mateusz/counties_choropleth/js/index.js
+++ mateusz/counties_choropleth/js/index.js
@@ -1,75 +1,120 @@
-var width = 960,
+
+// Define Globals
+var width,
+    height,
+    projection,
+    housing,
+    path,
+    svg,
+    color,
+    x,
+    attirbuteArray = [],
+    currentAttribute = 0,
+    play = false;
+
+function init() {
+    setMap();
+    // animateMap();
+}
+
+function setMap() {
+    width = 960;
     height = 960;
 
-var unemployment = d3.map();
+    projection = null; //to be defined
 
-var path = d3.geoPath();
+    housing = d3.map();
 
-var x = d3.scaleLinear()
-    .domain([1, 10])
-    .rangeRound([600, 860]);
+    path = d3.geoPath();
+    // .projection(projection)
 
-var color = d3.scaleThreshold()
-    .domain(d3.range(2, 10))
-    .range(d3.schemeBlues[9]);
+    x = d3.scaleLinear()
+        .domain([1, 10])
+        .rangeRound([600, 860]);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    color = d3.scaleThreshold()
+        .domain(d3.range(2, 10))
+        .range(d3.schemeBlues[9]);
 
-var g = svg.append("g")
-    .attr("class", "key")
-    .attr("transform", "translate(0,40)");
+    svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-g.selectAll("rect")
-    .data(color.range().map(function (d) {
-        d = color.invertExtent(d);
-        if (d[0] == null) d[0] = x.domain()[0];
-        if (d[1] == null) d[1] = x.domain()[1];
-        return d;
-    }))
-    .enter().append("rect")
-    .attr("height", 8)
-    .attr("x", function (d) { return x(d[0]); })
-    .attr("width", function (d) { return x(d[1]) - x(d[0]); })
-    .attr("fill", function (d) { return color(d[0]); });
+    g = svg.append("g")
+        .attr("class", "key")
+        .attr("transfrom", "translate(0,40");
 
-g.append("text")
-    .attr("class", "caption")
-    .attr("x", x.range()[0])
-    .attr("y", -6)
-    .attr("fill", "#000")
-    .attr("text-anchor", "start")
-    .attr("font-weight", "bold")
-    .text("Unemployment rate");
+    g.selectAll("rect")
+        .data(color.range().map(function (d) {
+            d = color.invertExtent(d);
+            if (d[0] == null) d[0] = x.domain()[0];
+            if (d[1] == null) d[1] = x.domain()[1];
+            return d;
+        }))
+        .enter().append("rect")
+        .attr("height", 8)
+        .attr("x", function (d) { return x(d[0]); })
+        .attr("width", function (d) { return x(d[1]) - x(d[0]); })
+        .attr("fill", function (d) { return color(d[0]); });
 
-g.call(d3.axisBottom(x)
-    .tickSize(13)
-    .tickFormat(function (x, i) { return i ? x : x + "%"; })
-    .tickValues(color.domain()))
-    .select(".domain")
-    .remove();
+    g.append("text")
+        .attr("class", "caption")
+        .attr("x", x.range()[0])
+        .attr("y", -6)
+        .attr("fill", "#000")
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .text("Unemployment rate");
 
-d3.queue()
-    .defer(d3.json, "https://d3js.org/us-10m.v1.json")
-    .defer(d3.tsv, "unemployment.tsv", function (d) { unemployment.set(d.id, +d.rate); })
-    .await(ready);
+    g.call(d3.axisBottom(x)
+        .tickSize(13)
+        .tickFormat(function (x, i) { return i ? x : x + "%"; })
+        .tickValues(color.domain()))
+        .select(".domain")
+        .remove();
 
-function ready(error, us) {
+    loadData();
+}
+
+function loadData() {
+    d3.queue()
+        .defer(d3.json, "https://d3js.org/us-10m.v1.json")
+        .defer(d3.csv, "County_Zhvi_AllHomes_Formatted.csv")
+        .await(processData);
+}
+
+function processData(error, topology, csv) {
     if (error) throw error;
+
+    console.log("us");
+    console.log(topology);
+
+    csv.forEach(function (d) { housing.set(d["CountyCodeFIPS"], +d["1996-04"]) })
 
     svg.append("g")
         .attr("class", "counties")
         .selectAll("path")
-        .data(topojson.feature(us, us.objects.counties).features)
+        .data(topojson.feature(topology, topology.objects.counties).features)
         .enter().append("path")
-        .attr("fill", function (d) { return color(d.rate = unemployment.get(d.id)); })
+        .attr("fill", function (d) { 
+            // console.log(d);
+            // console.log(housing);
+            var rate = housing.get(d.id);
+            if(rate == null){rate=0.0}; // otherwise those counties will have no color at all
+            return color(d.rate = rate); 
+        })
         .attr("d", path)
+        .on('mouseover', function(d){
+            // debug if id's of counties are accurate
+			var name = d.id;
+			console.log(d);
+		})
         .append("title")
         .text(function (d) { return d.rate + "%"; });
 
     svg.append("path")
-        .datum(topojson.mesh(us, us.objects.states, function (a, b) { return a !== b; }))
-        .attr("class", "states")
-        .attr("d", path);
+        .attr("class", "county-borders")
+        .attr("d", path(topojson.mesh(topology, topology.objects.counties, function (a, b) { return a !== b; })));
 }
+
+window.onload = init();
