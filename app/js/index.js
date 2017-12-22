0b5f3c65bf1ee35b4f7620985a3a5b65a89f9760
Christian Tresch <ch.tresch@gmail.com>
2017-12-22 11:44:06 -0500

    tooltip lock

--- app/js/index.js
+++ app/js/index.js
@@ -8,6 +8,16 @@ var tweenGroup2D3D = new TWEEN.Group();
 var INFO_TITLE = 'US REAL ESTATE PRICES AVERAGED OVER ALL HOMES PER COUNTY';
 var INFO_LOAD = '';
 
+
+//////////////////////////////////////////
+
+var sprite1;
+var texture1;
+var projector, mouse = { x: 0, y: 0 }, INTERSECTED;
+
+//////////////////////////////////////////
+
+
 var main = function () {
     width = window.innerWidth * scale;
     height = window.innerHeight * scale;
@@ -19,7 +29,7 @@ main();
 
 // DATA LOADING FUNCTIONS
 // loader settings
-var opts = {
+var spinnerOptions = {
     lines: 9, // The number of lines to draw
     length: 25, // The length of each line
     width: 8, // The line thickness
@@ -33,6 +43,16 @@ var opts = {
 var target = document.getElementById('container');
 var info = document.getElementById('info');
 
+// Define the div for the tooltip
+var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+// when the mouse moves, call the given function
+document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+
+
 function loadData() {
 
     queue()   // queue function loads all external data files asynchronously
@@ -45,7 +65,7 @@ function loadData() {
 function processData(error, counties, timeData) {
 
     // trigger loader
-    var spinner = new Spinner(opts).spin(target);
+    var spinner = new Spinner(spinnerOptions).spin(target);
     info.innerHTML = INFO_LOAD;
 
 
@@ -253,6 +273,44 @@ function init() {
 
     window.addEventListener( 'resize', onWindowResize, false );
 
+    // Tooltip section
+    // initialize object to perform world/screen calculations
+    projector = new THREE.Projector();
+
+    /////// draw text on canvas /////////
+    // create a canvas element
+    canvas1 = document.createElement('canvas');
+    context1 = canvas1.getContext('2d');
+    context1.font = "Bold 20px Arial";
+    context1.fillStyle = "rgba(0,0,0,0.95)";
+    context1.fillText('Hello, world!', 0, 20);
+
+    // canvas contents will be used for a texture
+    texture1 = new THREE.Texture(canvas1)
+    texture1.needsUpdate = true;
+
+    var spriteMaterial = new THREE.SpriteMaterial( { map: texture1, useScreenCoordinates: true } );
+    //var materialC = new THREE.SpriteMaterial( { map: mapC, color: 0xffffff, fog: true } );
+
+    sprite1 = new THREE.Sprite( spriteMaterial );
+    sprite1.scale.set(200,100,1.0);
+    sprite1.position.set( 50, 50, 0 );
+    scene.add( sprite1 );
+}
+
+// Tooltip mouse event
+function onDocumentMouseMove( event )
+{
+    // the following line would stop any other event handler from firing
+    // (such as the mouse's TrackballControls)
+    // event.preventDefault();
+
+    // update sprite position
+    sprite1.position.set( event.clientX, event.clientY - 20, 0 );
+
+    // update the mouse variable
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
 
 
@@ -338,6 +396,8 @@ function animate() {
 
     render();
 
+    tooltipUpdate();
+
 }
 
 function render() {
@@ -420,4 +480,76 @@ function animateMap() {
                 step();
             })
         );
+}
+
+
+function tooltipUpdate()
+{
+
+    // create a Ray with origin at the mouse position
+    //   and direction into the scene (camera direction)
+    var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
+    //projector.unprojectVector( vector, camera );
+    vector.unproject(camera);
+    var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+    // create an array containing all objects in the scene with which the ray intersects
+    var intersects = ray.intersectObjects( scene.children );
+
+    // INTERSECTED = the object in the scene currently closest to the camera
+    //		and intersected by the Ray projected from the mouse position
+
+    // if there is one (or more) intersections
+    if ( intersects.length > 0 )
+    {
+        // if the closest object intersected is not the currently stored intersection object
+        if ( intersects[ 0 ].object != INTERSECTED )
+        {
+            // restore previous intersection object (if it exists) to its original color
+            if ( INTERSECTED )
+                INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+            // store reference to closest object as current intersection object
+            INTERSECTED = intersects[ 0 ].object;
+            // store color of closest object (for later restoration)
+            INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+            // set a new color for closest object
+            INTERSECTED.material.color.setHex( 0xffff00 );
+
+            // update text, if it has a "name" field.
+            if ( intersects[ 0 ].object.name )
+            {
+                context1.clearRect(0,0,640,480);
+                var message = intersects[ 0 ].object.name;
+                var metrics = context1.measureText(message);
+                var width = metrics.width;
+                context1.fillStyle = "rgba(0,0,0,0.95)"; // black border
+                context1.fillRect( 0,0, width+8,20+8);
+                context1.fillStyle = "rgba(255,255,255,0.95)"; // white filler
+                context1.fillRect( 2,2, width+4,20+4 );
+                context1.fillStyle = "rgba(0,0,0,1)"; // text color
+                context1.fillText( message, 4,20 );
+                texture1.needsUpdate = true;
+            }
+            else
+            {
+                context1.clearRect(0,0,300,300);
+                texture1.needsUpdate = true;
+            }
+        }
+    }
+    else // there are no intersections
+    {
+        // restore previous intersection object (if it exists) to its original color
+        if ( INTERSECTED )
+            INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+        // remove previous intersection object reference
+        //     by setting current intersection object to "nothing"
+        INTERSECTED = null;
+        context1.clearRect(0,0,300,300);
+        texture1.needsUpdate = true;
+    }
+
+
+    controls.update();
+    stats.update();
 }
